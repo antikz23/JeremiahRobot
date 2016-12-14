e670fa92a0489942d245543e10df7be52c3a93ef
Matthew Sheehan <matthewjs0109@gmail.com>
2016-12-14 14:10:31 -0500

    Added code

---
+++ Jeremiah_Robot_Driving_Codes.ino
@@ -0,0 +1,33 @@
+#define RIGHTA 7
+#define RIGHTB 8
+#define LEFTA 9
+#define LEFTB 10
+
+void robot()
+{
+  digitalWrite(RIGHTA, HIGH);
+  digitalWrite(RIGHTB, LOW);
+  digitalWrite(LEFTA, HIGH);
+  digitalWrite(LEFTB, LOW);
+}
+void reverse()
+{
+  digitalWrite(RIGHTA, LOW);
+  digitalWrite(RIGHTB, HIGH);
+  digitalWrite(LEFTA, LOW);
+  digitalWrite(LEFTB, HIGH);
+}
+void setup()
+{
+  pinMode(RIGHTA, OUTPUT);
+  pinMode(RIGHTB, OUTPUT);
+  pinMode(LEFTA, OUTPUT);
+  pinMode(LEFTB, OUTPUT);
+}
+void loop()
+{
+  robot();
+  delay(3000);
+  reverse();
+  delay(3000);
+}
